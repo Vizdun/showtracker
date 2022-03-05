af4852ca2aa105f3f6fdd55d26e2595f3e5a4fdd
Vizdun <55970818+Vizdun@users.noreply.github.com>
2022-03-05 15:43:50 +0100

    more tolerant tracked show serialization

--- src/storage.rs
+++ src/storage.rs
@@ -6,7 +6,37 @@ use crate::structs::{Show, TrackedShow};
 
 #[derive(serde::Serialize, serde::Deserialize)]
 struct SerializedTrackedShows {
-    tracked_shows: Vec<TrackedShow>,
+    tracked_shows: Vec<SerializedTrackedShow>,
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct SerializedTrackedShow {
+    id: Option<u32>,
+    episode_count: Option<u16>,
+    name: Option<String>,
+}
+
+impl From<&SerializedTrackedShow> for TrackedShow {
+    fn from(show: &SerializedTrackedShow) -> Self {
+        TrackedShow {
+            id: show.id.unwrap_or(0),
+            episode_count: show.episode_count.unwrap_or(0),
+            name: show
+                .name
+                .clone()
+                .unwrap_or(String::from("null")),
+        }
+    }
+}
+
+impl From<&TrackedShow> for SerializedTrackedShow {
+    fn from(show: &TrackedShow) -> Self {
+        SerializedTrackedShow {
+            id: Some(show.id),
+            episode_count: Some(show.episode_count),
+            name: Some(show.name.clone()),
+        }
+    }
 }
 
 pub fn load_show_list() -> Vec<Show> {
@@ -45,12 +75,19 @@ pub fn load_tracked_shows() -> Vec<TrackedShow> {
     let serialized_tracked_shows: SerializedTrackedShows =
         toml::from_slice(&track_list_bin).unwrap();
 
-    serialized_tracked_shows.tracked_shows
+    serialized_tracked_shows
+        .tracked_shows
+        .iter()
+        .map(|x| TrackedShow::from(x))
+        .collect()
 }
 
 pub fn save_tracked_shows(track_list: Vec<TrackedShow>) {
     let serialized_tracked_shows = SerializedTrackedShows {
-        tracked_shows: track_list,
+        tracked_shows: track_list
+            .iter()
+            .map(|x| SerializedTrackedShow::from(x))
+            .collect(),
     };
 
     let track_list_bin =
