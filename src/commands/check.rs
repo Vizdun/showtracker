a02691a17e66c9d9739d5b0e699cd45648ef159c
Vizdun <55970818+Vizdun@users.noreply.github.com>
2022-03-05 18:53:36 +0100

    fixed check query

--- src/commands/check.rs
+++ src/commands/check.rs
@@ -1,39 +1,83 @@
-use crate::{common::get_request, storage::*};
+use scraper::{Html, Selector};
+
+use crate::{
+    common::get_request, storage::*, structs::TrackedShow,
+};
+
+fn query(track_list: &Vec<TrackedShow>) -> String {
+    format!(
+        r#"SELECT ?show ?episodeCount
+WHERE
+{{
+  VALUES ?show {{{}}}.
+  ?show wdt:P1113 ?episodeCount.
+}}"#,
+        (&track_list)
+            .iter()
+            .map(|track| format!("wd:Q{}", track.id))
+            .collect::<Vec<String>>()
+            .join(" ")
+    )
+}
 
 pub fn check() {
     let mut track_list = load_tracked_shows();
 
-    let checked_shows = get_request(&format!(
-            "https://query.wikidata.org/sparql?query=SELECT%20%3FepisodeCount%0AWHERE%0A%7B%0A%20%20VALUES%20%3Fshow%20%7Bwd%3A{}%0A%20%20%3Fshow%20wdt%3AP1113%20%3FepisodeCount.%0A%7D",
-            (&track_list)
-            .iter()
-            .map(|track|track.id.to_string())
-            .collect::<Vec<String>>()
-            .join("%20wd%3A"))
-        )
-    .split("<literal datatype='http://www.w3.org/2001/XMLSchema#decimal'>")
-    .collect::<Vec<&str>>()[1..]
-    .iter()
-    .enumerate()
-    .map(|(id, result)| {
-        return (id, result.splitn(2, "</literal>")
-        .collect::<Vec<&str>>()[0]
-        .parse::<u16>().unwrap())
-    })
-    .collect::<Vec<(usize, u16)>>();
+    let xml = get_request(&format!(
+        "https://query.wikidata.org/sparql?query={}",
+        urlencoding::encode(&query(&track_list))
+    ));
 
-    for track_tuple in checked_shows {
-        if track_list[track_tuple.0].episode_count
-            < track_tuple.1
-        {
+    let fragment = Html::parse_fragment(&xml);
+    let result_selector =
+        Selector::parse("result").unwrap();
+    let show_id_selector =
+        Selector::parse("binding[name=show] uri").unwrap();
+    let episode_count_selector = Selector::parse(
+        "binding[name=episodeCount] literal",
+    )
+    .unwrap();
+
+    let checked_shows = fragment
+        .select(&result_selector)
+        .map(|x| {
+            (
+                x.select(&show_id_selector)
+                    .next()
+                    .unwrap()
+                    .text()
+                    .collect::<String>()
+                    .split_once("Q")
+                    .unwrap()
+                    .1
+                    .parse::<u32>()
+                    .unwrap(),
+                x.select(&episode_count_selector)
+                    .next()
+                    .unwrap()
+                    .text()
+                    .collect::<String>()
+                    .parse::<u16>()
+                    .unwrap(),
+            )
+        })
+        .collect::<Vec<(u32, u16)>>();
+
+    track_list
+        .sort_by(|a, b| a.id.partial_cmp(&b.id).unwrap());
+
+    for (id, track) in checked_shows {
+        let indx = track_list
+            .binary_search_by(|probe| probe.id.cmp(&id))
+            .unwrap();
+        if track_list[indx].episode_count < track {
             println!(
                 "New episode of {}",
-                track_list[track_tuple.0].name
+                track_list[indx].name
             );
         }
 
-        track_list[track_tuple.0].episode_count =
-            track_tuple.1;
+        track_list[indx].episode_count = track;
     }
 
     save_tracked_shows(track_list);
