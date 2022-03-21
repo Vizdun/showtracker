3ff713b2e43b37243fe84e63324465f47a4c8c8c
Vizdun <>
2022-03-21 09:22:21 +0100

    clippy and fmt

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ mod main;
 fn main() -> std::io::Result<()> {
     let out_dir = std::path::PathBuf::from(
         std::env::var_os("OUT_DIR")
-            .ok_or_else(|| std::io::ErrorKind::NotFound)?,
+            .ok_or(std::io::ErrorKind::NotFound)?,
     );
 
     let cmd = main::Cli::command();
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 use clap::Parser;
 
+mod cli;
 mod commands;
 mod common;
 mod storage;
 mod structs;
-mod cli;
 
 fn main() {
     let cli = cli::Cli::parse();
 
     match &cli.command {
         cli::Commands::Update => commands::update(),
-        cli::Commands::Search { search_term, max, regex } => {
-            commands::search(search_term, max, *regex)
-        }
+        cli::Commands::Search {
+            search_term,
+            max,
+            regex,
+        } => commands::search(search_term, max, *regex),
         cli::Commands::Track { id } => commands::track(id),
-        cli::Commands::Untrack { id } => commands::untrack(id),
+        cli::Commands::Untrack { id } => {
+            commands::untrack(id)
+        }
         cli::Commands::List => commands::list(),
         cli::Commands::Check => commands::check(),
     }
